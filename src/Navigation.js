0be1b7faee09d9fe7f307f272c0a6b5e9fb0d7be
lakshay <bhardwajlakshay545@gmail.com>
2024-06-11 23:01:34 +0530

    navigation done

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,23 +3,37 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ProductDetailsScreen from "./screens/ProductDetailsScreen";
 import ShoppingCartScreen from "./screens/ShoppingCartScreen";
 import ProductScreen from "./screens/ProductScreen";
+import { Pressable, Text } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{animation:'slide_from_bottom'}}>
+      <Stack.Navigator screenOptions={{ animation: "slide_from_bottom" , contentStyle:{backgroundColor:'white'}}}>
         <Stack.Screen
           name="Products"
           component={ProductScreen}
-          options={{ headerTitleAlign: "center" }}
+          options={({ navigation }) => ({
+            headerTitleAlign: "center",
+            headerRight: () => (
+              <Pressable
+                style={{ flexDirection: "row" }}
+                onPress={() => {
+                  navigation.navigate("Cart");
+                }}
+              >
+                <FontAwesome5 name="shopping-cart" size={18} color="grey" />
+                <Text style={{ marginLeft: 5 }}>1</Text>
+              </Pressable>
+            ),
+          })}
         />
         <Stack.Screen
           name="Product Details"
           component={ProductDetailsScreen}
-          options={{ headerTitleAlign: "center"}}
-          
+          options={{ headerTitleAlign: "center" }}
         />
         <Stack.Screen
           name="Cart"
